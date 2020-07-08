9723ddad85bd40bd898c04eb32dc742566e77018
annatimofeyeva <anjatimofeyeva@gmail.com>
2020-07-08 16:36:08 -0700

    Listeners class added

--- src/main/java/JavaExercises/Strings.java
+++ src/main/java/JavaExercises/Strings.java
@@ -1,4 +1,16 @@
 package JavaExercises;
 
 public class Strings {
+    public static void main(String[] args){
+        // примитивнвй тип - хранит данные
+        int n = 4;
+        // ссылается на обьект класса String
+        String s = "Hello";
+        //"Hello" - обьект класса String
+        // переменная s ссылается на обьект "Hello"
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s + space + name);
+        System.out.println("Hello " + " " + "John");
+    }
 }
