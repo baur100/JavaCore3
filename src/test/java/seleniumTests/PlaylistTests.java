8bb98ef6b2cbdcca2ac9c9191fc0715f7882eef4
Michael Krasickiy <krasickiymichael@gmail.com>
2020-07-17 21:13:51 +0300

    Homeworks

--- src/test/java/seleniumTests/PlaylistTests.java
+++ src/test/java/seleniumTests/PlaylistTests.java
@@ -2,27 +2,28 @@ package seleniumTests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pageObjects.MainPage;
 
 public class PlaylistTests extends BaseTest{
 
     @Test
-    public void playlistTests_createPlaylist_PlaylistCreated(){
+    public void playlistTests_createPlaylist_PlaylistCreated(MainPage mainPage){
         String playlistId = mainPage.createPlaylist("xxxxx");
         Assert.assertTrue(mainPage.checkPlaylist(playlistId));
     }
     @Test
-    public void playlistTests_renamePlaylist_PlaylistRenamed(){
+    public void playlistTests_renamePlaylist_PlaylistRenamed(MainPage mainPage){
         String playlistId = mainPage.createPlaylist("xxxxx");
         mainPage.renamePlaylist(playlistId,"newName");
         Assert.assertTrue(mainPage.checkPlaylist(playlistId,"newName"));
     }
     @Test
-    public void playlistTests_createPlaylist_PlaylistCreated1(){
+    public void playlistTests_createPlaylist_PlaylistCreated1(MainPage mainPage){
         String playlistId = mainPage.createPlaylist("xxxxx");
         Assert.assertTrue(mainPage.checkPlaylist(playlistId));
     }
     @Test
-    public void playlistTests_renamePlaylist_PlaylistRenamed1(){
+    public void playlistTests_renamePlaylist_PlaylistRenamed1(MainPage mainPage){
         String playlistId = mainPage.createPlaylist("xxxxx");
         mainPage.renamePlaylist(playlistId,"newName");
         Assert.assertTrue(mainPage.checkPlaylist(playlistId,"newName"));
