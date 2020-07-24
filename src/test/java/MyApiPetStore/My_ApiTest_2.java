9dfb1f9740f71c504fa83052845ac15d2b530907
Michael Krasickiy <krasickiymichael@gmail.com>
2020-07-24 06:26:46 +0300

    JAVA_API

--- src/test/java/MyApiPetStore/My_ApiTest_2.java
+++ src/test/java/MyApiPetStore/My_ApiTest_2.java
@@ -2,7 +2,12 @@ package MyApiPetStore;
 
 import My_models.My_Category;
 import My_models.My_GetPetResponse;
+import helpers.TestData;
+import helpers.TestObjectCreator;
 import io.restassured.response.Response;
+import models.Category;
+import models.GetPetResponse;
+import models.PostPetRequest;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
@@ -12,7 +17,9 @@ import java.util.List;
 import static io.restassured.RestAssured.given;
 
 public class My_ApiTest_2 {
-    @Test
+    private long petId;
+    private PostPetRequest petRequest;
+    @Test(enabled=false)
     public void getPet(){
         Response response = given()
                 .baseUri("https://petstore.swagger.io/v2")
@@ -39,10 +46,8 @@ public class My_ApiTest_2 {
 
         Assert.assertEquals(pet.getStatus(), "available");
         Assert.assertEquals(pet.getName(), "piper");
-
-
     }
-    @Test
+    @Test(enabled=false)
     public void getByStatus(){
         Response response = given()
                 .baseUri("https://petstore.swagger.io/v2")
@@ -58,5 +63,70 @@ public class My_ApiTest_2 {
         List<My_GetPetResponse> list = Arrays.asList(jsonPath.getObject("$", My_GetPetResponse[].class));
         System.out.println(list.size());
         System.out.println(list.get(0).getName());
+    }
+    @Test(priority=1)
+    public void createPet(){
+        petRequest = TestObjectCreator.createPostRetRequest();
+        Response response = given()
+                .baseUri("https://petstore.swagger.io/v2")
+                .basePath("/pet")
+                .header("Content-Type","application/json")
+                .body(petRequest)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        var jsonPath = response.jsonPath();
+
+        GetPetResponse pet = jsonPath.getObject("$",GetPetResponse.class);
+        petId = pet.getId();
+
+        Assert.assertEquals(pet.getStatus(),petRequest.getStatus());
+        Assert.assertEquals(pet.getName(),petRequest.getName());
+        Assert.assertEquals(pet.getCategory().getName(),petRequest.getCategory().getName());
+        Assert.assertEquals(pet.getCategory().getId(),petRequest.getCategory().getId());
+//        var headers = response.getHeaders();
+//        System.out.println(headers.size());
+    }
+    @Test(priority=2)
+    public void updatePetName(){
+        String newName = TestData.randomString(12);
+        petRequest.setId(petId);
+        petRequest.setName(newName);
+        Response response = given()
+                .baseUri("https://petstore.swagger.io/v2")
+                .basePath("/pet")
+                .header("Content-Type","application/json")
+                .body(petRequest)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        var jsonPath = response.jsonPath();
+
+        GetPetResponse pet = jsonPath.getObject("$",GetPetResponse.class);
+
+        Assert.assertEquals(pet.getStatus(),petRequest.getStatus());
+        Assert.assertEquals(pet.getName(),newName);
+        Assert.assertEquals(pet.getCategory().getName(),petRequest.getCategory().getName());
+        Assert.assertEquals(pet.getCategory().getId(),petRequest.getCategory().getId());
+        Assert.assertEquals(pet.getId(),petId);
+    }
+    @Test(priority=3)
+    public void deletePetName(){
+        Response response = given()
+                .baseUri("https://petstore.swagger.io/v2")
+                .basePath("/pet/+petId")
+                .header("Content-Type","application/json")
+                .when()
+                .delete()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
     }
 }
